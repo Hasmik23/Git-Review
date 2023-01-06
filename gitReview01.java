c271ad77c24dab7375ee6b79b055d01e6c347f18
ht.ohannesian@gmail.com <shirag.ohannesian@gmail.com>
2023-01-05 20:27:52 -0500

    a loop that printa numbers from 0 to 50 with an increment of 5

--- gitReview01.java
+++ gitReview01.java
@@ -2,8 +2,8 @@ public class gitReview01 {
     public static void main(String[] args) {
 
 
-        for(int 1=0;i<100;i=i+5){
-            System.out.println(i);
+        for(int 1=0;i<20;i=i+5){
+            System.out.println(i);  // git commit -m "a for loop to print numbers from 0 to 100 with a gap of 5"
         }
     }
 }
